84178d35a9ea3065d583a6ae44c6496516d37f39
imanahmedoslo <imanahmedimanahmed@gmail.com>
2023-03-23 14:38:21 +0100

    Update loginController.js

--- src/loginPage/loginController.js
+++ src/loginPage/loginController.js
@@ -36,7 +36,7 @@ function login() {
                     model.app.isLoggedIn=true
                     break;
                 case "student":
-                case "pendingStudent":
+                case "pendingStudentProfile":
                     model.app.page="studentProfile"
                     model.app.isLoggedIn=true
                     break;  
