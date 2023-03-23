212e2f7485a48a958b2509682a0ad46388c3121f
sjojon <90796535+sjojon@users.noreply.github.com>
2023-03-23 09:56:21 +0100

    Oppdatert index

--- src/acceptPendingStudentPage/acceptPendingStudentView.js
+++ src/acceptPendingStudentPage/acceptPendingStudentView.js
@@ -1,7 +1,7 @@
 function pendingAcceptStudentView() {
     const acceptStudent = model.data.accounts.allAccounts.student;
     return /*HTML*/ `
-    <link rel="stylesheet" href="src/acceptPendingStudentPage/acceptPendingStudentStyle.css" />;
+    
     
     `;
 }
