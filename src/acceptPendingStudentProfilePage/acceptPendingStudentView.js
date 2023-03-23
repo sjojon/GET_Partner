08e0f545db42099dd512068cdb2877a9e0a260ba
sjojon <90796535+sjojon@users.noreply.github.com>
2023-03-23 12:46:37 +0100

    oppdatert

--- src/acceptPendingStudentProfilePage/acceptPendingStudentView.js
+++ src/acceptPendingStudentProfilePage/acceptPendingStudentView.js
@@ -1,9 +1,16 @@
 function acceptPendingStudentView() {
     const acceptStudent = model.data.accounts.allAccounts.filter(obj => obj.userType === 'student');
+    // console.log(acceptStudent);
     return /*HTML*/ `
-    console.log(acceptStudent);
-    
+    <div>
+        <div>
+        ${isStudentProfilePendingListEmpty()}
+        </div>
+    </div>
     `;
 }
 
-'\n    console.log(acceptStudent);\n    \n    ';
+function isStudentProfilePendingListEmpty() {
+    const acceptStudent = model.data.accounts.allAccounts.filter(obj => obj.userType === 'student');
+    if (myObj.isEmpty())
+}
