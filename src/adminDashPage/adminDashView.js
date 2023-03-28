aebf2610b0e7c7f86f49d437a94f4ae552a585d0
thesneilert <eilert.gaming@outlook.com>
2023-03-28 10:02:39 +0200

    grid admin

--- src/adminDashPage/adminDashView.js
+++ src/adminDashPage/adminDashView.js
@@ -2,21 +2,26 @@
 
 function adminDashView() {
   return /*html*/ `
-    <div id="adminDash-box-outer">
-        <div id="adminDash-tittel-text">Totalt antall brukere:</div>
-            <div class="adminDash-info-box">
-                <div class="adminDash-box-name">Ventende firmaer: ${countPendingCompany}</div>
-            </div>
-            <div class="adminDash-info-box">
-                <div class="adminDash-box-name">Studenter: ${countStudents}</div>
-            </div>
-            <div class="adminDash-info-box">
-                <div class="adminDash-box-name">Firmaer: ${countCompanies}</div>
-            </div>
-            <div class="adminDash-info-box">    
-                <div class="adminDash-box-name">Administrator: ${countAdmins}</div>
-            </div>
-        </div>
-    </div>   
+<div class="adminDashGrid">
+  <div class="adminDash-box-outer">
+      <div class="adminDash-tittel-text">Totalt antall brukere:</div>
+      <div class="adminDash-info-box">
+          <div class="adminDash-box-name">Ventende firmaer: ${countPendingCompany}</div>
+      </div>
+      <div class="adminDash-info-box">
+          <div class="adminDash-box-name">Studenter: ${countStudents}</div>
+      </div>
+      <div class="adminDash-info-box">
+          <div class="adminDash-box-name">Firmaer: ${countCompanies}</div>
+      </div>
+      <div class="adminDash-info-box">    
+          <div class="adminDash-box-name">Administrator: ${countAdmins}</div>
+      </div>
+  </div>
+
+  <div class="adminDash-box-outer">
+  </div>
+</div>
+
     `;
 }
