6896c61aa3c501786163bd0456b87bbb5ab8aed9
VictorVariety <victorv@getacademy.no>
2023-03-29 14:19:09 +0200

    Dummy data fixes i model og studentPage 95% ferdig

--- src/studentPage/studentView.js
+++ src/studentPage/studentView.js
@@ -1,28 +1,52 @@
-// students = model.data.accounts.allAccounts.filter(obj => obj.userType === 'student') // for studentBrowseSearch
-
-
 function browseStudentView() {
     let techList = findTechsXpByAccountId(parseInt(model.inputs.studentSearchPage.studentId)) 
     let student = model.data.accounts[2] //DUMMY
     return /*HTML*/`
-    <div class="outer">
-        <div class="left">
-            <div class="student-name">Navn: ${student.name}</div>
-            <div class="student-pic">${student.pic}</div>
-            <div class="bosted">Addresse: ${student.address}</div>
-            <div class="tech-xp">Tech: ${createTechListHtml(techList)}</div>
-            <div class="xp">Viktig informasjon: ${student.importantInfo}</div>
-            <div class="interests">Interesser: ${student.interests}</div>
-            <div class="email">Email: ${student.email}</div>
+    <div class="student-outer">
+        <div class="student-inner">
+            <div class="student-info">
+                <div class="student-name">${student.name}</div>
+                <img class="student-pic" src="${student.pic}">
+                <div class="student-address">
+                    Addresse: 
+                    <div class="student-address-inner">${student.address}</div>
+                </div>
+                <div class="student-email">
+                    Email: 
+                    <div class="student-email-inner">${student.email}</div>
+                </div>
+                <div class="student-tech-xp">
+                    Kode erfaring: 
+                    <div class="student-tech-xp-inner">${createTechListHtml(techList)}</div>
+                </div>
+                <div class="student-xp">
+                    Informasjon: 
+                    <div class="student-xp-inner">${student.importantInfo}</div>
+                </div>
+                <div class="student-interests">
+                    Interesser: 
+                    <div class="student-interests-inner">${student.interests}</div>
+                </div>
+            </div>
+            <div class="student-map">
+                <div class="map">
+                    X MARKS THE SPOT
+                </div>
+            </div>
+            <div class="student-links">
+                <img width="25px" style="margin-bottom: 4px" src="images/Github_logo.png" >
+                <a class="github-link" href="${student.gitHub}" target="_new">
+                    Github
+                </a> 
+                <a class="cv-link" href="${student.cv}">
+                    CV
+                </a>
+            </div>
+            <div class="student-buttons">
+                <button class="triangle-button triangle-left" onclick=""></button> 
+                <button class="triangle-button triangle-right" onclick=""></button>
+            </div>
         </div>
-        <div class="links">
-            <a href="${student.gitHub}" target="_new">githubPlaceholder</a>
-            <div>${student.cv}</div>
-        </div>
-        <div class="right">
-            <div class="map"></div>
-        </div>
-
     </div>
     
     `}
