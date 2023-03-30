2beb84bd8a8143c9fe6c72e332e5f08bb5a766ca
thesneilert <eilert.gaming@outlook.com>
2023-03-31 01:26:52 +0200

    bootstrap og header design

--- src/adminDashPage/adminDashController.js
+++ src/adminDashPage/adminDashController.js
@@ -3,15 +3,19 @@ let countStudents = 0;
 let countCompanies = 0;
 let countPendingCompany = 0;
 let countAdmins = 0;
-
+let countProfileChange = 0;
 for (let account of model.data.accounts) {
   if (account.userType === 'student') {
     countStudents++;
   } else if (account.userType === 'company') {
     countCompanies++;
-    } else if (account.userType === 'pendingCompany') {
+  } else if (account.userType === 'pendingCompany') {
     countPendingCompany++;
   } else if (account.userType === 'admin') {
     countAdmins++;
+  } else if (account.usertype === '') {
+    countProfileChange++;
   }
 }
+
+//alle goTo funksjoner ved knappeklikk
--- src/adminDashPage/adminDashView.js
+++ src/adminDashPage/adminDashView.js
@@ -1,27 +1,69 @@
-//pendingCompanyAccounts, pendingProfileChange, createStudentAccount
 
 function adminDashView() {
-  return /*html*/ `
-<div class="adminDashGrid">
-  <div class="adminDash-box-outer">
-      <div class="adminDash-tittel-text">Totalt antall brukere:</div>
-      <div class="adminDash-info-box">
-          <div class="adminDash-box-name">Ventende firmaer: ${countPendingCompany}</div>
-      </div>
-      <div class="adminDash-info-box">
-          <div class="adminDash-box-name">Studenter: ${countStudents}</div>
-      </div>
-      <div class="adminDash-info-box">
-          <div class="adminDash-box-name">Firmaer: ${countCompanies}</div>
-      </div>
-      <div class="adminDash-info-box">    
-          <div class="adminDash-box-name">Administrator: ${countAdmins}</div>
-      </div>
-  </div>
-
-  <div class="adminDash-box-outer">
-  </div>
-</div>
-
+    return /*html*/ `
+        <div class="adminDashGrid">
+          <div class="adminDash-box-outer">
+            <div class="adminDash-info-box">
+            <div class="adminDash-box-name">${adminDashShowAlertIconCompany()}&nbsp;&nbsp;Ventende firmaer: ${countPendingCompany}</div>
+            <div class="adminDash-box-name">${adminDashShowAlertIconStudent()}&nbsp;&nbsp;Student profil endr: ${countProfileChange}</div>
+            <br>
+              <div class="adminDash-box-name"><i class="bi bi-building"></i>&nbsp;&nbsp;Firmaer: ${countCompanies}</div> 
+              <div class="adminDash-box-name"><i class="bi bi-person"></i>&nbsp;&nbsp;Studenter: ${countStudents}</div> 
+              <div class="adminDash-box-name"><i class="bi bi-person-workspace"></i>&nbsp;&nbsp;Administrator: ${countAdmins}</div>
+            </div>
+          </div>
+          <div class="adminDash-box-outer2">
+            <div id="adminDash-button-grid-box">
+                <button class="adminDash-button-box" onclick="">Godk. student profil endr.</button>
+                <button class="adminDash-button-box" onclick="">Godkjenne bedrifter</button>
+                <button class="adminDash-button-box" onclick="">Opprett ny student bruker</button>
+                <button class="adminDash-button-box" onclick="">Opprett ny bedrift konto</button>
+                <button class="adminDash-button-box" onclick="">Fjern en student konto</button>
+                <button class="adminDash-button-box" onclick="">Fjern en bedrift konto</button>
+                <button class="adminDash-button-box" onclick="">knapp</button>
+                <button class="adminDash-button-box" onclick="">knapp</button>
+            </div>
+          </div>
+          <div class="adminDash-box-outer3">
+            <div id="adminDash-button-grid-box3">
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+                <button class="adminDash-button-box" onclick=""></button>
+            </div>
+          </div>
+        </div>
     `;
+}
+
+//bytter ikon om det er pendingCompanies eller studentChanges
+function adminDashShowAlertIconCompany(){
+    if (countPendingCompany > 0) {
+        return /*html*/ `<i style="color:red;" class="bi bi-exclamation-circle"></i>`;
+    } else {
+        return /*html*/ `<i class="bi bi-building-gear"></i>`;
+    }
+}
+function adminDashShowAlertIconStudent(){
+    if (countProfileChange > 0) {
+        return /*html*/ `<i style="color:red;" class="bi bi-exclamation-circle"></i>`;
+    } else {
+        return /*html*/ `<i class="bi bi-person-gear"></i>`;
+    }
 }
