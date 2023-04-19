f078a858fe2901ba31b1fceadf8b6742876ce4a4
VictorVariety <victorv@getacademy.no>
2023-04-19 14:02:39 +0200

    Model update & studentBrowseSearch Upload

---
+++ src/studentBrowsePage/studentBrowseController.js
@@ -0,0 +1,26 @@
+
+function handleInputChange() {
+    let input = document.getElementById('input');
+    let searchOptions = document.getElementById('student-search-options');
+    model.inputs.studentSearchPage.search = input.value;
+    searchOptions.innerHTML = createSearchParametersHtml();
+}
+
+function addFilter(parameter) {
+    currentFilters.push(parameter)
+    let index = searchParameters.indexOf(parameter)
+    searchParameters.splice(index, 1)
+
+    model.inputs.studentSearchPage.search = ''
+    updateView()
+}
+
+function removeFilter(index) {
+    let removedFilter = currentFilters.splice(index, 1)[0]
+    searchParameters.push(removedFilter)
+    updateView()
+}
+
+function showStudentPage(){
+    
+}
