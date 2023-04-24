6dd5b349fd413fdb368c0b6ad0ef6b3852c2d6b1
thesneilert <eilert.gaming@outlook.com>
2023-04-24 10:52:49 +0200

    fikset feil funksjon saveProfile()

--- src/registerStudent/registerStudentView.js
+++ src/registerStudent/registerStudentView.js
@@ -63,8 +63,47 @@ function registerStudentView() {
             </div>        
         </div>
         <div id="bottom-margin-rsv">
-            <button id="reg-button-rsv" onclick="registerCompanyButton()">Registrer</button>
+            <button id="reg-button-rsv" onclick="saveProfile()">Registrer</button>
             <br> <div id="error-text-rsv">${errorMessage}</div>
         </div>   
     </div> 
     `}
+
+
+
+
+    // function registerStudentView() {
+    //     return /*html*/ `
+    //       <link rel="stylesheet" href="src/registerStudent/registerStudentStyle.css" />
+    //       <div class="pageMinusHeader">
+    //         <div class="outer-box">
+    //           <h3 class="pageHeading">Registrer student</h3>
+      
+    //           <label for="navn" class="info-element">Navn</label><br>
+    //           <input type="text" name="navn" class="input-element" value="${registerNewStAccount.name}" onchange="registerNewStAccount.name=this.value"><br>
+      
+      
+    //           <label for="e-post" class="info-element">E-mail</label> <br>
+    //           <input type="text" name="e-post" class="input-element" value="${registerNewStAccount.email}" onchange="registerNewStAccount.email=this.value"><br>
+      
+      
+    //           <label for="passord" class="info-element">Passord</label> <br>
+    //           <input type=${switchInputType} name="passord" class="input-element passord" value="${registerNewStAccount.password}" onchange="registerNewStAccount.password=this.value"><br>
+      
+      
+    //           <label for="gjenta-passord" class="info-element">Gjenta passord</label> <br>
+    //           <input type=${switchInputType} name="gjenta-passord" class="input-element passord" value="${registerNewStAccount.repeatedPassword}" onchange="registerNewStAccount.repeatedPassword=this.value"><br>
+      
+      
+    //           <div class="showPasswordDiv">
+    //             <input type="checkbox" name="show-password" class="checkBox" onclick="showPassword(this)">
+    //             <label for="show-password" class="info-element labelCheckBox">Vis passord</label> 
+    //           </div>
+      
+              
+    //           <button onclick="saveProfile()" class="profileButton">Opprett profil</button>
+    //         </div>
+    //       </div>
+    //     `;
+    //   }
+      
