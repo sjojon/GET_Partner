4ead9bb0faea0ad275478235a5fc0768404d2b4f
hanscjo <johnsen.hans.christian@gmail.com>
2023-04-25 02:14:52 +0200

    companyLandingPage cirka ferdig dersom Eilert ikke begynner å grine av cssen

--- src/companyLandingPage/companyLandingPageView.js
+++ src/companyLandingPage/companyLandingPageView.js
@@ -4,11 +4,16 @@ function companyLandingPageView() {
 
     return /*HTML*/ `
     <div id="companyPageWrapper">
-        <div id="getPartnerInfo">
-            GET er insane bra!!!
-        </div>    
-        <div id="companyPageMapContainer">
-            <div id="map">
+        <div id="companyPageInner">
+            <div id="getPartnerInfo">
+                <h2>Velkommen til GET-partner!</h2>
+                <p>Ved hjelp av GET-partner finner du akkurat de studentene du trenger til bedriften din.</p> 
+                <p>GET sine studenter bor over hele landet, og du finner kontaktinformasjonen deres ved hjelp av verktøyet.</p>
+                <p id="companyLandingPageSearchLink" onclick="goToStudentSearchFilter()">Søk etter studenter</p>
+            </div>    
+            <div id="companyPageMapContainer">
+                <div id="map">
+                </div>
             </div>
         </div>
     </div>`
--- src/loginPage/loginController.js
+++ src/loginPage/loginController.js
@@ -55,7 +55,7 @@ function login() {
                     model.data.currentUser.id=accountsToBeChecked[key].id
                     break;  
                 case "company":
-                    model.app.page = "studentBrowseSearch"
+                    model.app.page = "companyLandingPage"
                     model.app.isLoggedIn = true
                     model.data.currentUser.id=accountsToBeChecked[key].id
                     break;
